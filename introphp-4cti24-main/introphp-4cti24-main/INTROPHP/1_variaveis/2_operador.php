adf6cd5b9cde2f2bd3a245a8e7e5cf7f641a3947
Gustavo <125514295+Gustavoaguiarlima@users.noreply.github.com>
2024-03-13 22:09:54 -0300

    Add files via upload

---
+++ introphp-4cti24-main/introphp-4cti24-main/INTROPHP/1_variaveis/2_operador.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$e= $a + $b; 
+echo '<br> A soma é ' . $c;
+$d = $a * $b;
+echo '<br>A multiplicacao é' . $d;
+
+$f = $a - $b;
+echo '<br>A divisao é' . $f;
+
+$g = $a % / $b;
+echo '<br>O médulo é ' . $g;
+?>
