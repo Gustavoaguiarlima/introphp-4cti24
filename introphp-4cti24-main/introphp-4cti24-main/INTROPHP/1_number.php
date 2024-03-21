946454eaf97a802942c61defd75e67bc80d40c4f
Gustavo <125514295+Gustavoaguiarlima@users.noreply.github.com>
2024-03-20 22:05:23 -0300

    Add files via upload

---
+++ introphp-4cti24-main/introphp-4cti24-main/INTROPHP/1_number.php
@@ -0,0 +1,12 @@
+<?php
+   /*variaveis */
+   $idade = 17; //inteiro
+   $nome = "Guilherme Ribeiro"; //sting
+   $estuda = null; //boleno
+   $salario = 0.00;
+
+
+echo "Ola me chamo" . $nome . "e tenho" . $idade . "anos";
+
+echo 'Estudo:' . $estuda . '' salário' . $salario;
+?>
---
+++ introphp-4cti24-main/introphp-4cti24-main/INTROPHP/2_operador.php
@@ -0,0 +1,23 @@
+<?php
+   
+   $a = 24;
+   $b = 16;
+
+   $c=  $a + $b;
+   echo ' A soma é ' . $c;
+
+   $d = $a * $b;
+   echo '<br> A multiplicação é ' . $d;
+
+
+   $e = $a * $b;
+   echo '<br>A subtracao é ' . $d;
+
+
+   $f = $a * $b;
+   echo '<br>A divisao é ' . $d;
+
+   $g = $a * $b;
+   echo '<br>O modulo  é ' . $d;
+
+   ?>
